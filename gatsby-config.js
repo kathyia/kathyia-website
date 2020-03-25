645f426f119a0bc9f0716410270801c1f951eca7
KIA <kat.ing.and@gmail.com>
2020-03-24 21:33:10 -0500

    first gatsby commit new repo

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `kathyia.com`,
+    description: `Kathy Andersen's website.'`,
+    author: `@kathyia`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#428bca`,
+        theme_color: `#428bca`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon-1024.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
